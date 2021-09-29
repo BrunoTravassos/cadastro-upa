5f6998756462db70cbe90627f91e247d5c34b937
BrunoTravassos <brunodefrontin@gmail.com>
2021-09-29 14:06:13 -0300

    commit criacao metodos no service

--- src/app/pacientes/index.ts
+++ src/app/pacientes/index.ts
@@ -1,1 +1,2 @@
 export * from './pacientes.module'
+export * from './shared'
--- src/app/pacientes/pacientes.module.ts
+++ src/app/pacientes/pacientes.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PacienteService } from '.';
+import { ListarPacienteComponent } from './listar/listar-paciente.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarPacienteComponent
+  ],
   imports: [
     CommonModule
+  ],
+  providers: [
+    PacienteService
   ]
 })
 export class PacientesModule { }
